9ddae0f85b6b46aa0b2635e022071fea551386e0
Anita Liberatore <anitaliberatore@hotmail.com>
2022-05-08 15:19:59 +0200

    push my example for create web server with node core

---
+++ cap2/cap2myExample.js
@@ -0,0 +1,73 @@
+console.log("Creating a Web Server with Node Core - three")
+
+const url = require('url')
+const http = require('http')
+const PORT = process.env.PORT || 3000
+const { STATUS_CODES } = http
+
+const myExample = `<html> 
+<head> 
+    <style> 
+        body { background: #FFF; margin: 1.25rem } h1 { color: #000; font-family: sans-serif } 
+    </style> 
+</head> 
+
+<body> 
+    <h1>My example</h1> 
+</body> 
+
+</html>`
+
+const urlBasic = `<html> 
+<head> 
+    <style> 
+        body { background: #FFF; margin: 1.25rem } h1 { color: #000; font-family: sans-serif } 
+    </style> 
+</head> 
+
+<body> 
+    <h1>Url basic</h1> 
+</body> 
+
+</html>`
+
+const hello = `<html> 
+<head> 
+    <style> 
+        body { background: #FFF; margin: 1.25rem } h1 { color: #000; font-family: sans-serif } 
+    </style> 
+</head> 
+
+<body> 
+    <h1>Hello</h1> 
+</body> 
+
+</html>`
+
+/*createServer method*/
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+
+    const { pathname } = url.parse(req.url)
+
+    if (pathname === '/my-example') {
+        res.end(myExample)
+        return
+    }
+
+    if (pathname === '/hello') {
+        res.end(hello)
+        return
+    } 
+
+    if (pathname === '/') {
+        res.end(urlBasic)
+        return
+    } 
+
+    res.statusCode = 404
+    res.end(STATUS_CODES[res.statusCode])
+    
+})
+console.log("Start server - port", PORT)
+server.listen(PORT)
