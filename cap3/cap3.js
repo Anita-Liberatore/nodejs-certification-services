747d29ed1b1cf327616ef849cdd96a003a0c4e11
Anita Liberatore <anitaliberatore@hotmail.com>
2022-05-30 09:55:19 +0200

    create web server with express

--- cap3/cap3.js
+++ cap3/cap3.js
@@ -1,1 +1,25 @@
-console.log("Cap 3")
+console.log("Cap 3 - Web server with express")
+const express = require('express')
+
+const createError = require('http-errors')
+const data = require('../cap3/data')
+const app = express()
+const PORT = 4000;
+
+app.use('/data', data)
+
+app.use((req, res, next) => {
+    if (req.method !== 'GET') {
+        console.log("Method 405")
+        next(createError(405))
+        return
+    }
+    console.log("not found")
+    next(createError(404))
+})
+
+
+app.listen(PORT, () => 
+    console.log(`Your server is running on port ${PORT} `)
+);
+module.exports = app
